df85f76a65d94480b43da1ef0fb7db4a45702ba7
Alexander Peltzer <alexander.peltzer@uni-tuebingen.de>
2018-02-21 14:59:46 +0100

    Basic Mothur image

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#This is required to build the stuff
+
+set -euo pipefail
+
+#Add here what kind of dependencies you need in general
+#Check for packages here: https://pkgs.alpinelinux.org/packages
+apk --update add bash wget curl build-base gcc git python-dev ca-certificates py-pip freetype-dev libpng libpng-dev readline
+
+#The actual tool installation should be done in the Singularity file itself to make the entire thing portable! 
+
+#Create mountpoints that we need
+mkdir -p /lustre_cfc/qbic/megSAP-data
+
+#Clean up
+rm -rf /var/cache/apk/*
+
+cd /
+rm -rf /build
